615a4192279a5a0c2fed62b6b9658887547ade7d
Sukho Lim - macbook pro <sssukho@gmail.com>
2021-10-23 01:33:55 +0900

    disruptor log server project set

---
+++ disruptor-log-server/src/main/java/com/sssukho/disruptorlogserver/DisruptorLogServerApplication.java
@@ -0,0 +1,13 @@
+package com.sssukho.disruptorlogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisruptorLogServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisruptorLogServerApplication.class, args);
+	}
+
+}
---
+++ disruptor-log-server/src/test/java/com/sssukho/disruptorlogserver/DisruptorLogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sssukho.disruptorlogserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisruptorLogServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
